0925281bbdbc62d19536e5e0d82d0c5881e19d33
cybersphere7866-star <cybersphere7866@gmail.com>
2026-07-08 10:20:24 +0530

    Module Program

---
+++ Classwork/Modules_8th_July/example1.py
@@ -0,0 +1,13 @@
+#----------------------------
+import numeric_calculator
+#----------------------------
+a=5
+b=10
+print("Sum of a and b is:",numeric_calculator.calculate_addtion(a,b))
+#----------------------------
+print("Difference of a and b is:",numeric_calculator.calculate_subtraction(a,b))
+#----------------------------
+print("Product of a and b is:",numeric_calculator.calculate_multiplication(a,b))
+#----------------------------
+print("Division of a and b is:",numeric_calculator.calculate_division(a,b))
+#----------------------------
---
+++ Classwork/Modules_8th_July/example2.py
@@ -0,0 +1,9 @@
+from numeric_calculator import *
+#------------------------------------------
+a=5
+b=10
+print ("Sum of a and b is:",calculate_addtion(a,b) )
+print ("Difference of a and b is:",calculate_subtraction(a,b) )
+print ("Product of a and b is:",calculate_multiplication(a,b) )
+print ("Division of a and b is:",calculate_division(a,b) )
+#----------------------------------------
