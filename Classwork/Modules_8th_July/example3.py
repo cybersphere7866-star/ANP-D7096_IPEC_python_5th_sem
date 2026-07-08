6ee66c266075a0c0a414a9b2b1b958be566f86ab
cybersphere7866-star <cybersphere7866@gmail.com>
2026-07-08 10:51:35 +0530

    Example 4

---
+++ Classwork/Modules_8th_July/example3.py
@@ -0,0 +1,10 @@
+from numeric_calculator import calculate_addtion
+#------------------------------------------
+a=5
+b=10
+print ("Sum of a and b is:",calculate_addtion(a,b) )
+m=50
+n=40
+print("Difference of m and n is:",calculate_subtraction(m,n) )
+#Output: Difference of m and n is: 10
+#Error: name 'calculate_subtraction' is not defined
---
+++ Classwork/Modules_8th_July/example4.py
@@ -0,0 +1,6 @@
+#importing module 
+import numeric_calculator as nc
+#----------------------------------------
+a=5 
+b=15
+print("Sum of a and b is:",nc.calculate_addtion(a,b))
